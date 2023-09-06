6b0e7dcfaf7355b252a86601c2e7855b90c85a55
be-krishna <bkshgnj@gmail.com>
2023-09-06 16:15:21 +0530

    created layouts and fragments and Post Model

--- social_posts/models.py
+++ social_posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.URLField(max_length=500)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        primary_key=True,
+        max_length=100,
+        default=True,
+        unique=True,
+        editable=False,
+    )
--- social_posts/views.py
+++ social_posts/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    title = "Welcom to Django"
-    return render(request, "index.html", {"title": title})
+    print(request.META)
+    return render(request, "social_posts/home.html")
